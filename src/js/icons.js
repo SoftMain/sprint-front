c6dea74603a668c5f50b18d44dadaa891e0bc7cb
yanin-artem <108901382+yanin-artem@users.noreply.github.com>
2022-11-14 20:07:12 +0300

    pre final

--- src/js/icons.js
+++ src/js/icons.js
@@ -1,7 +1,24 @@
-let icons = document.querySelectorAll('svg');
+let icons = document.querySelectorAll('svg'); //ищем все svg
+let reg_exp = /^(\d{2}px)$/;  //регулярное выражение
 
-for(let icon of icons){
-    alert(icon.classList[0]);
+for(let icon of icons){ //перебираем по каждому svg
+    if(icon.classList[1]&&reg_exp.test(icon.classList[1])){//если существует второй параметр класса и он соответствует регулярке то, размеры вводятся динамически из класса
+        icon.style.cssText = `
+        width: ${icon.classList[1]};
+        height: ${icon.classList[1]};                                                       
+        background: url(../img/${icon.classList[0]}.svg) center center/cover no-repeat;
+        `
+    }
+    else{ //если второго параметра нет или на его месте какой-то пользовательский класс, то размеры шаблонные
+        icon.style.cssText = `
+        width: 24px;
+        height: 24px;
+        background: url(../img/${icon.classList[0]}.svg) center center/cover no-repeat;
+        `
+    }
 }
+
+
+
 
 
