4ba44af60c0fbcd2884769d1c19c8c384ddbfd1c
yanin-artem <108901382+yanin-artem@users.noreply.github.com>
2022-11-18 00:06:43 +0300

    icon fixed

--- src/js/icons.js
+++ src/js/icons.js
@@ -3,19 +3,47 @@ let reg_exp = /^(\d{2}px)$/; //регулярное выражение
 
 for (let icon of icons) {
   //перебираем по каждому svg
+  let css = `width: ${icon.classList[1]};height: ${icon.classList[1]};background: url(../img/icons/${icon.classList[0]}.svg) center center/cover no-repeat;`;
+  
   if (icon.classList[1] && reg_exp.test(icon.classList[1])) {
-    //если существует второй параметр класса и он соответствует регулярке то,размеры вводятся динамически из класса
-    icon.style.cssText = `
-        width: ${icon.classList[1]};
-        height: ${icon.classList[1]};                                                       
-        background: url(./img/icons/${icon.classList[0]}.svg) center center/cover no-repeat;
+    switch(icon.classList[1]){
+      case '64px':
+        icon.style.cssText = css;
+        break;
+      case '48px':
+        icon.style.cssText = css;
+        break;
+      case '32px':
+        icon.style.cssText = css;
+        break;
+      case '24px':
+        icon.style.cssText = css;
+        break;
+      case '20px':
+        icon.style.cssText = css;
+        break;
+      case '16px':
+        icon.style.cssText = css;
+        break;
+      case '14px':
+        icon.style.cssText = css;
+        break;
+      default:{
+        icon.style.cssText = `
+        width: 24px;
+        height: 24px;
+        background: url(../img/icons/error.svg) center center/cover no-repeat;
         `;
+      }
+      
+    }
+    
   } else {
     //если второго параметра нет или на его месте какой-то пользовательский класс, то размеры шаблонные
     icon.style.cssText = `
         width: 24px;
         height: 24px;
-        background: url(./img/icons/${icon.classList[0]}.svg) center center/cover no-repeat;
+        background: url(../img/icons/${icon.classList[0]}.svg) center center/cover no-repeat;
         `;
   }
 }
