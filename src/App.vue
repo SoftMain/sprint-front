d6d80ae0f31e9666a9cf7e988bcbbfca870fa92a
Andrey Paliy <awbait.com@gmail.com>
2022-11-22 22:54:55 +0300

    Page router

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <template>
   <Header />
-  <Main />
+  <router-view></router-view>
   <Footer />
 </template>
 
 <script setup>
   import Header from './components/Header.vue';
   import Footer from './components/Footer.vue';
-  import Main from './views/Main.vue';
-
 </script>
 
 <style lang="scss">
---
+++ src/router.js
@@ -0,0 +1,51 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import Main from './views/Main.vue';
+import Catalog from './views/Catalog.vue';
+import Product from './views/Product.vue';
+import Integrators from './views/Integrators.vue';
+import Contact from './views/Contact.vue';
+import About from './views/About.vue';
+import Error404 from './views/404.vue';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'main',
+      component: Main
+    },
+    {
+      path: '/catalog',
+      name: 'catalog',
+      component: Catalog
+    },
+    {
+      path: '/product',
+      name: 'product',
+      component: Product
+    },
+    {
+      path: '/integrators',
+      name: 'integrators',
+      component: Integrators
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: Contact
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: About
+    },
+    {
+      path: '/error',
+      name: 'error',
+      component: Error404
+    }
+  ]
+});
+
+export default router
