e1ad788b6adbe9ad36af8acd316517160c0fc1d7
Andrey Paliy <awbait.com@gmail.com>
2022-11-24 03:16:30 +0300

    fix

--- src/components/Categoryes/Chapters.vue
+++ src/components/Categoryes/Chapters.vue
@@ -1,12 +1,13 @@
 <template>
   <ul class="blockcategory-navmenu-navigation">
-    <li v-for="chapter in chapters" 
-    :key="chapter.id" 
-    @click="testClick(chapter.id)" 
-    :class="{ active: chapter.id == activeCategory }"
+    <li
+      v-for="chapter in chapters"
+      :key="chapter.id"
+      @click="chapterChange(chapter.id)"
+      :class="{ active: chapter.id == activeCategory }"
     >
       <span :class="chapter.icon + ' px20'"></span>
-        <h6>{{ chapter.text }}</h6>
+      <h6 class="text-semibold">{{ chapter.text }}</h6>
     </li>
   </ul>
 </template>
@@ -22,17 +23,17 @@ export default {
   emits: ["cur"],
   data() {
     return {
-      activeCategory: 1
+      activeCategory: 1,
     };
   },
   methods: {
     filter() {
       this.isActive = !this.isActive;
     },
-    testClick(id) {
-      this.$emit('cur', id)
+    chapterChange(id) {
+      this.$emit("cur", id);
       this.activeCategory = id;
-    }
+    },
   },
 };
 </script>
--- src/components/Categoryes/Product.vue
+++ src/components/Categoryes/Product.vue
@@ -1,34 +1,33 @@
 <template>
-    <div v-for="post in posts" class="category-container">
-            <div class="category-container-logo"><img :src="'img/productlogo/'+post.id+'.png'" alt="">
-            </div>
-            <div class="category-container-main">
-              <div class="category-container-main-title">
-                <div class="category-container-main-title-name">
-                  <h6>{{post.title}}</h6>
-                </div>
-                <a :href="post.link" class="category-container-main-title-button">
-                  <span class="icon-chevron-r px16"></span>
-                </a>
-              </div>
-              <div class="category-container-main-description base">
-                {{post.text}}
-              </div>
-            </div>
-          </div>
+  <div v-for="post in posts" class="category-container">
+    <div class="category-container-logo">
+      <img :src="`img/products/${post.id}.png`" alt="" />
+    </div>
+    <div class="category-container-main">
+      <div class="category-container-main-title">
+        <div class="category-container-main-title-name">
+          <h6>{{ post.title }}</h6>
+        </div>
+        <a :href="post.link" class="category-container-main-title-button">
+          <span class="icon-chevron-r px16"></span>
+        </a>
+      </div>
+      <div class="category-container-main-description base">
+        {{ post.text }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        props:{
-          posts:{
-            type:Array,
-            required:true
-          }
-        }
-    }
+export default {
+  props: {
+    posts: {
+      type: Array,
+      required: true,
+    },
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
