da32adcb48042f57aa9c4727e0957db186d50470
Xellan <xellanbox@gmail.com>
2022-11-25 21:07:47 +0300

    первая пачка заданий

---
+++ src/components/Contact/InfoBlock.vue
@@ -0,0 +1,28 @@
+<template>
+    <section>
+        <div class="infoblock container">
+            <div class="infoblock-logo">
+                <img src="img/background/company_page.svg" alt="" />
+            </div>
+            <div class="infoblock-text">
+                <div class="infoblock-text1">
+                    <h2>
+                        Чтобы разместить на сайте свой продукт, напишите нам через форму
+                        ниже
+                    </h2>
+                </div>
+                <div class="infoblock-text2 large">
+                    Мы обязательно с вами свяжемся и уточним детали!
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Contact/Mail.vue
@@ -0,0 +1,20 @@
+<template>
+    <section>
+        <div class="mail container">
+            <div class="mail-text-one">
+                <h4>У вас остались вопросы? Скорее напишите их нам!</h4>
+            </div>
+            <div class="mail-text-two">
+                <h2>hello@softmain.ru</h2>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
