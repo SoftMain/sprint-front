0a65169610607bb79afcc7af494c1aabd91be534
yanin-artem <108901382+yanin-artem@users.noreply.github.com>
2022-11-26 23:13:09 +0300

    -

--- src/views/Catalog.vue
+++ src/views/Catalog.vue
@@ -143,7 +143,7 @@
     </section>
 
     <section>
-      <div class="main">
+      <div class="catalog">
         <div class="sort-button">
           <a class="btn btn-primary">Высокий рейтинг<span class="icon-caret-down px20"></span></a>
         </div>
