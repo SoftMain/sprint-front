6e71189182c1c7de437a980ff32b3dfdd19999fa
Kachan <ninakartoskina4@gmail.com>
2022-11-26 21:56:19 +0300

    add sidebar styles

--- src/views/Catalog.vue
+++ src/views/Catalog.vue
@@ -4,39 +4,112 @@
 
       <div class="sidebar">
         <div class="sidebar__categories">
-          <div class="sidebar__h6"></div>
-          <div class="sidebar__catalog-categories"></div>
+          <div class="sidebar__h6 font-regular text-lg">
+            <h6>Категория</h6>
+          </div>
+          <div class="sidebar__catalog-categories">
+            <p><span class="icon-chevron-l px16"></span>Антивирусы и безопасность</p>
+            <a class="btn btn-primary btn-sm">Антивирусы для дома</a>
+          </div>
         </div>
 
         <div class="sidebar__filters">
           <div class="sidebar__platform-filter">
             <div class="sidebar__collapse">
-              <div class="sidebar__collapse-icon"></div>
-              <div class="sidebar__collapse-h6"></div>
+              <div class="sidebar__collapse-icon">
+                <span class="icon-caret-down px20"></span>
+              </div>
+              <div class="sidebar__collapse-h6">
+                <h6>Платформа</h6>
+              </div>
             </div>
             <div class="sidebar__variants">
               <div class="sidebar__checkbox">
-                <div class="sidebar__checkbox-icon"></div>
-                <div class="sidebar__checkbox-text"></div>
+                <div class="sidebar__checkbox-icon">
+                  <span class="icon-checkbox-fill px15"></span>
+                </div>
+                <div class="sidebar__checkbox-text">
+                  <p>Windows</p>
+                </div>
+              </div>
+              <div class="sidebar__checkbox">
+                <div class="sidebar__checkbox-icon">
+                  <span class="icon-checkbox px15"></span>
+                </div>
+                <div class="sidebar__checkbox-text">
+                  <p>Linux</p>
+                </div>
+              </div>
+              <div class="sidebar__checkbox">
+                <div class="sidebar__checkbox-icon">
+                  <span class="icon-checkbox px15"></span>
+                </div>
+                <div class="sidebar__checkbox-text">
+                  <p>Android</p>
+                </div>
+              </div>
+              <div class="sidebar__checkbox">
+                <div class="sidebar__checkbox-icon">
+                  <span class="icon-checkbox px15"></span>
+                </div>
+                <div class="sidebar__checkbox-text">
+                  <p>iOS</p>
+                </div>
               </div>
             </div>
           </div>
           <div class="sidebar__certificate-filter">
             <div class="sidebar__collapse">
-              <div class="sidebar__collapse-icon"></div>
-              <div class="sidebar__collapse-h6"></div>
+              <div class="sidebar__collapse-icon">
+                <span class="icon-caret-down px20"></span>
+              </div>
+              <div class="sidebar__collapse-h6">
+                <h6>Сертификация</h6>
+              </div>
             </div>
             <div class="sidebar__variants">
               <div class="sidebar__checkbox">
-                <div class="sidebar__checkbox-icon"></div>
-                <div class="sidebar__checkbox-text"></div>
+                <div class="sidebar__checkbox-icon">
+                  <span class="icon-checkbox-fill px15"></span>
+                </div>
+                <div class="sidebar__checkbox-text">
+                  <p>Windows</p>
+                </div>
+              </div>
+              <div class="sidebar__checkbox">
+                <div class="sidebar__checkbox-icon">
+                  <span class="icon-checkbox px15"></span>
+                </div>
+                <div class="sidebar__checkbox-text">
+                  <p>Linux</p>
+                </div>
+              </div>
+              <div class="sidebar__checkbox">
+                <div class="sidebar__checkbox-icon">
+                  <span class="icon-checkbox px15"></span>
+                </div>
+                <div class="sidebar__checkbox-text">
+                  <p>Android</p>
+                </div>
+              </div>
+              <div class="sidebar__checkbox">
+                <div class="sidebar__checkbox-icon">
+                  <span class="icon-checkbox px15"></span>
+                </div>
+                <div class="sidebar__checkbox-text">
+                  <p>iOS</p>
+                </div>
               </div>
             </div>
           </div>
           <div class="sidebar__language-filter">
             <div class="sidebar__collapse">
-              <div class="sidebar__collapse-icon"></div>
-              <div class="sidebar__collapse-h6"></div>
+              <div class="sidebar__collapse-icon">
+                <span class="icon-caret-up px20"></span>
+              </div>
+              <div class="sidebar__collapse-h6">
+                <h6>Язык интерфейса</h6>
+              </div>
             </div>
             <div class="sidebar__variants">
               <div class="sidebar__checkbox">
@@ -47,8 +120,12 @@
           </div>
           <div class="sidebar__analogs-filter">
             <div class="sidebar__collapse">
-              <div class="sidebar__collapse-icon"></div>
-              <div class="sidebar__collapse-h6"></div>
+              <div class="sidebar__collapse-icon">
+                <span class="icon-caret-up px20"></span>
+              </div>
+              <div class="sidebar__collapse-h6">
+                <h6>Аналоги</h6>
+              </div>
             </div>
             <div class="sidebar__variants">
               <div class="sidebar__checkbox">
@@ -58,8 +135,8 @@
             </div>
           </div>
           <div class="sidebar__actions">
-            <div class="sidebar__button"></div>
-            <div class="sidebar__button"></div>
+              <a class="btn btn-primary">Применить</a>
+              <a class="btn btn-outline-primary">Сбросить</a>
           </div>
         </div>
       </div>
@@ -250,15 +327,15 @@
             <div class="products__header-container">
               <div class="products__image">
                 <img
-                  src="https://content.kaspersky-labs.com/se/ru/content/ru-ru/images/kaspersky-logo-square/kaspersky-logo-square.svg"
+                  src="https://upload.wikimedia.org/wikipedia/ru/3/3a/The_Elder_Scrolls_V_-_Skyrim.jpg"
                   alt="">
               </div>
               <div class="products__content">
                 <div class="products__title font-regular text-lg">
-                  <h4>Kaspersky Internet Security</h4>
+                  <h4>TES: Skyrim</h4>
                 </div>
                 <div class="products__developer font-light text-sm">
-                  <p>Kaspersky</p>
+                  <p>Bethesda</p>
                   <span class="icon-verify px16"></span>
                 </div>
                 <div class="products__rating">
@@ -266,8 +343,14 @@
                   <span class="icon-star-filled px20"></span>
                   <span class="icon-star-filled px20"></span>
                   <span class="icon-star-filled px20"></span>
-                  <span class="icon-star px20"></span>
-                  <p class="text-sm">(12)</p>
+                  <span class="icon-star-filled px20"></span>
+                  <span class="icon-star-filled px20"></span>
+                  <span class="icon-star-filled px20"></span>
+                  <span class="icon-star-filled px20"></span>
+                  <span class="icon-star-filled px20"></span>
+                  <span class="icon-star-filled px20"></span>
+
+                  <p class="text-sm">(9999999999)</p>
                 </div>
               </div>
             </div>
@@ -277,14 +360,11 @@
           </div>
           <div class="products__description">
             <p class="text-sm">
-              Антивирусное программное обеспечение, разрабатываемое «Лабораторией Касперского». Предоставляет
-              пользователю защиту от вирусов, троянских программ, шпионских программ, руткитов, adware, а также от
-              неизвестных угроз с помощью проактивной защиты, включающей компонент HIPS. Первоначально, в начале 1990-х,
-              именовался -V, затем - AntiViral Toolkit Pro.</p>
+              Купи скайрим.</p>
           </div>
           <div class="products__under">
             <div class="products__button">
-              <a class="btn btn-primary btn-sm">Подробнее</a>
+              <a class="btn btn-primary btn-sm">Купить скайрим</a>
             </div>
           </div>
         </div>
