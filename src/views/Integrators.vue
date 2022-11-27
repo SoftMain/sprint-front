ff21da98e46f8fa632057a587dfec7f40db363ba
Andrey Paliy <awbait.com@gmail.com>
2022-11-28 00:02:28 +0300

    fix integrators image path

--- src/views/Integrators.vue
+++ src/views/Integrators.vue
@@ -1,60 +1,129 @@
 <template>
-        <div class="container">
-            <div class="hero proto_block">
-                <div class="hero__text_block">
-                    <div class="hero__text_block_header"><h2 class="font-medium">Что такое системный интегратор?</h2></div>
-                    <div class="hero__text_block_text"><p class="text-base"> Системный интегратор в IT –это компания, которая разрабатывает и внедряет комплексные IT-решения для автоматизации бизнес-процессов на предприятии.
-                        Основной задачей системных интеграторов является обеспечение эффективной работы отдельной команды (отдела) или всей организации в целом. Системные интеграторы занимаются поддержкой и интеграцией: ПО, аппаратных комплексов, решений (ERP, CRM), систем в бизнес-процессах.
-                        Услуги системной интеграции предоставляются подрядными компаниями, которые имеют в своем штате высококвалифицированных и сертифицированных сотрудников, способных решить любые задачи, поставленные заказчиком. Ниже вы можете ознакомиться с ведущими системными интеграторами в России и их клиентами.</p></div>
-                </div>
-                <div class="hero__image"><img src="../../public/img/Integrators image/integrators_hero.png" alt=""></div>
-            </div>
+  <div class="container">
+    <div class="hero proto_block">
+      <div class="hero__text_block">
+        <div class="hero__text_block_header">
+          <h2 class="font-medium">Что такое системный интегратор?</h2>
         </div>
-        <div class="integrators proto_block">
-            <div class="integrators_block_header"><h3 class="font-medium">Список Интерграторов</h3></div>
-            <div class="integrator__block_cards">
-                <div class="integrator__block_cards_elem">
-                    <div class="integrator__block_cards_elem_image"><img src="../../public/img/Integrators image/softline.png" alt=""></div>
-                    <div class="integrator__block_cards_elem_header"><h5 class="font-regular">SOFTLINE</h5></div>
-                    <div class="integrator__block_cards_elem_text"><p class="text-base">Доброфлот, Росводоканал</p></div>
-                </div>
-                <div class="integrator__block_cards_elem">
-                    <div class="integrator__block_cards_elem_image"><img src="../../public/img/Integrators image/lanit.png" alt=""></div>
-                    <div class="integrator__block_cards_elem_header"><h5 class="font-regular">Ланит</h5></div>
-                    <div class="integrator__block_cards_elem_text"><p class="text-base">Куратье, Росгидромет</p></div>
-                </div>
-                <div class="integrator__block_cards_elem">
-                    <div class="integrator__block_cards_elem_image"><img src="../../public/img/Integrators image/krok.png" alt=""></div>
-                    <div class="integrator__block_cards_elem_header"><h5 class="font-regular">КРОК</h5></div>
-                    <div class="integrator__block_cards_elem_text"><p class="text-base">Ингосстрах, ДИТ Москвы</p></div>
-                </div>
-                <div class="integrator__block_cards_elem">
-                    <div class="integrator__block_cards_elem_image"><img src="../../public/img/Integrators image/galacticka.png" alt=""></div>
-                    <div class="integrator__block_cards_elem_header"><h5 class="font-regular">Корпорация Галактика</h5></div>
-                    <div class="integrator__block_cards_elem_text"><p class="text-base">Росатом, Роснефть</p></div>
-                </div>
-                <div class="integrator__block_cards_elem">
-                    <div class="integrator__block_cards_elem_image"><img src="../../public/img/Integrators image/itglobal.png" alt=""></div>
-                    <div class="integrator__block_cards_elem_header"><h5 class="font-regular">ITGLOBAL.COM</h5></div>
-                    <div class="integrator__block_cards_elem_text"><p class="text-base">Сеть АЗС «Опти», Digital Attitude</p></div>
-                </div>
-                <div class="integrator__block_cards_elem">
-                    <div class="integrator__block_cards_elem_image"><img src="../../public/img/Integrators image/aiteko.png" alt=""></div>
-                    <div class="integrator__block_cards_elem_header"><h5 class="font-regular">АЙТЕКО</h5></div>
-                    <div class="integrator__block_cards_elem_text"><p class="text-base">Райффайзенбанк, Москоллектор</p></div>
-                </div>
-                <div class="integrator__block_cards_elem">
-                    <div class="integrator__block_cards_elem_image"><img src="../../public/img/Integrators image/otr.png" alt=""></div>
-                    <div class="integrator__block_cards_elem_header"><h5 class="font-regular">OTP</h5></div>
-                    <div class="integrator__block_cards_elem_text"><p class="text-base">ПФР, МинФин, Альфа-Банк, Ренессанс</p></div>
-                </div>
-                <div class="integrator__block_cards_elem">
-                    <div class="integrator__block_cards_elem_image"><img src="../../public/img/Integrators image/korus.png" alt=""></div>
-                    <div class="integrator__block_cards_elem_header"><h5 class="font-regular">КОРУС Консалтинг</h5></div>
-                    <div class="integrator__block_cards_elem_text"><p class="text-base">РЖД, Газпромнефть, ВТБ Лизинг, Азбука вкуса, Магнит</p></div>
-                </div>
-            </div>
+        <div class="hero__text_block_text">
+          <p class="text-base">
+            Системный интегратор в IT –это компания, которая разрабатывает и
+            внедряет комплексные IT-решения для автоматизации бизнес-процессов
+            на предприятии. Основной задачей системных интеграторов является
+            обеспечение эффективной работы отдельной команды (отдела) или всей
+            организации в целом. Системные интеграторы занимаются поддержкой и
+            интеграцией: ПО, аппаратных комплексов, решений (ERP, CRM), систем в
+            бизнес-процессах. Услуги системной интеграции предоставляются
+            подрядными компаниями, которые имеют в своем штате
+            высококвалифицированных и сертифицированных сотрудников, способных
+            решить любые задачи, поставленные заказчиком. Ниже вы можете
+            ознакомиться с ведущими системными интеграторами в России и их
+            клиентами.
+          </p>
         </div>
+      </div>
+      <div class="hero__image">
+        <img src="/img/Integrators image/integrators_hero.png" alt="" />
+      </div>
+    </div>
+  </div>
+  <div class="integrators proto_block">
+    <div class="integrators_block_header">
+      <h3 class="font-medium">Список Интерграторов</h3>
+    </div>
+    <div class="integrator__block_cards">
+      <div class="integrator__block_cards_elem">
+        <div class="integrator__block_cards_elem_image">
+          <img src="/img/Integrators image/softline.png" alt="" />
+        </div>
+        <div class="integrator__block_cards_elem_header">
+          <h5 class="font-regular">SOFTLINE</h5>
+        </div>
+        <div class="integrator__block_cards_elem_text">
+          <p class="text-base">Доброфлот, Росводоканал</p>
+        </div>
+      </div>
+      <div class="integrator__block_cards_elem">
+        <div class="integrator__block_cards_elem_image">
+          <img src="/img/Integrators image/lanit.png" alt="" />
+        </div>
+        <div class="integrator__block_cards_elem_header">
+          <h5 class="font-regular">Ланит</h5>
+        </div>
+        <div class="integrator__block_cards_elem_text">
+          <p class="text-base">Куратье, Росгидромет</p>
+        </div>
+      </div>
+      <div class="integrator__block_cards_elem">
+        <div class="integrator__block_cards_elem_image">
+          <img src="/img/Integrators image/krok.png" alt="" />
+        </div>
+        <div class="integrator__block_cards_elem_header">
+          <h5 class="font-regular">КРОК</h5>
+        </div>
+        <div class="integrator__block_cards_elem_text">
+          <p class="text-base">Ингосстрах, ДИТ Москвы</p>
+        </div>
+      </div>
+      <div class="integrator__block_cards_elem">
+        <div class="integrator__block_cards_elem_image">
+          <img src="/img/Integrators image/galacticka.png" alt="" />
+        </div>
+        <div class="integrator__block_cards_elem_header">
+          <h5 class="font-regular">Корпорация Галактика</h5>
+        </div>
+        <div class="integrator__block_cards_elem_text">
+          <p class="text-base">Росатом, Роснефть</p>
+        </div>
+      </div>
+      <div class="integrator__block_cards_elem">
+        <div class="integrator__block_cards_elem_image">
+          <img src="/img/Integrators image/itglobal.png" alt="" />
+        </div>
+        <div class="integrator__block_cards_elem_header">
+          <h5 class="font-regular">ITGLOBAL.COM</h5>
+        </div>
+        <div class="integrator__block_cards_elem_text">
+          <p class="text-base">Сеть АЗС «Опти», Digital Attitude</p>
+        </div>
+      </div>
+      <div class="integrator__block_cards_elem">
+        <div class="integrator__block_cards_elem_image">
+          <img src="/img/Integrators image/aiteko.png" alt="" />
+        </div>
+        <div class="integrator__block_cards_elem_header">
+          <h5 class="font-regular">АЙТЕКО</h5>
+        </div>
+        <div class="integrator__block_cards_elem_text">
+          <p class="text-base">Райффайзенбанк, Москоллектор</p>
+        </div>
+      </div>
+      <div class="integrator__block_cards_elem">
+        <div class="integrator__block_cards_elem_image">
+          <img src="/img/Integrators image/otr.png" alt="" />
+        </div>
+        <div class="integrator__block_cards_elem_header">
+          <h5 class="font-regular">OTP</h5>
+        </div>
+        <div class="integrator__block_cards_elem_text">
+          <p class="text-base">ПФР, МинФин, Альфа-Банк, Ренессанс</p>
+        </div>
+      </div>
+      <div class="integrator__block_cards_elem">
+        <div class="integrator__block_cards_elem_image">
+          <img src="/img/Integrators image/korus.png" alt="" />
+        </div>
+        <div class="integrator__block_cards_elem_header">
+          <h5 class="font-regular">КОРУС Консалтинг</h5>
+        </div>
+        <div class="integrator__block_cards_elem_text">
+          <p class="text-base">
+            РЖД, Газпромнефть, ВТБ Лизинг, Азбука вкуса, Магнит
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
