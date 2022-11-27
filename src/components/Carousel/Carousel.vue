be7298d4512607064fc80dfc4774d7b7c8aa03f4
Andrey Paliy <awbait.com@gmail.com>
2022-11-27 11:32:35 +0300

    refactoring partners block

---
+++ src/components/Carousel/Carousel.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="shuffle__image">
+    <CarouselSlide
+      v-for="(slide, index) in slides"
+      :key="slide"
+      :index="index"
+      :visibleSlide="visibleSlide"
+      :direction="direction"
+    >
+      <img :class="slide" alt="" />
+    </CarouselSlide>
+  </div>
+</template>
+
+<script>
+import CarouselSlide from "../Carousel/CarouselSlide.vue";
+
+export default {
+  props: ["slidesIn"],
+  components: { CarouselSlide },
+  data() {
+    return {
+      directions: ["left", "right", "top", "down"],
+      slides: [],
+      visibleSlide: 0,
+      direction: 'left',
+    }
+  },
+  methods: {
+    slide() {
+      this.direction = this.directions[Math.floor(Math.random()*this.directions.length)];
+      if (this.visibleSlide == 8) {
+        this.visibleSlide = 0;
+        return;
+      }
+      this.visibleSlide++;
+    },
+  },
+  mounted() {
+    this.slides = this.slidesIn.map((a) => [Math.random(),a]).sort((a,b) => a[0]-b[0]).map((a) => a[1]);
+    let interval = (Math.floor(Math.random() * 10) + 3) * 1000
+    console.log(interval)
+    setInterval(() => this.slide(), interval);
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
