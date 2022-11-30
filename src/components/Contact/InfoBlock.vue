a5368ccd8f3e947dff6ba46c4cac879fcbb1a2cc
Xellan <xellanbox@gmail.com>
2022-11-30 22:51:03 +0300

    адапт страницы "компаниям", под мобилки не особо пашет, но стили прописаны

--- src/components/Contact/InfoBlock.vue
+++ src/components/Contact/InfoBlock.vue
@@ -1,20 +1,19 @@
 <template>
     <section>
-        <div class="infoblock container">
+        <div class="infoblock">
             <div class="infoblock-logo">
-                <img src="/img/background/company_page.svg" alt="" />
+                <img class="infoblock-logo-pic" src="/img/background/company_page.svg" alt="" />
             </div>
-            <div class="infoblock-text">
-                <div class="infoblock-text1">
-                    <h2>
-                        Чтобы разместить на сайте свой продукт, напишите нам через форму
-                        ниже
-                    </h2>
-                </div>
-                <div class="infoblock-text2 large">
-                    Мы обязательно с вами свяжемся и уточним детали!
-                </div>
+            <div class="infoblock-text1">
+                <h2 class="infoblock-text1-h">
+                    Чтобы разместить на сайте свой продукт, напишите нам через форму
+                    ниже
+                </h2>
             </div>
+            <div class="infoblock-text2 large infoblock-text2-size">
+                Мы обязательно с вами свяжемся и уточним детали!
+            </div>
+
         </div>
     </section>
 </template>
