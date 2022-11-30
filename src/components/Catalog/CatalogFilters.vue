2d1aaf7b42116c20b35a58230735f316a0a03d52
Kachan <ninakartoskina4@gmail.com>
2022-11-30 13:24:52 +0300

    split catalog sidebar to modules

---
+++ src/components/Catalog/CatalogFilters.vue
@@ -0,0 +1,24 @@
+<template>
+  <CatalogFilter v-for="(filter, index) in filters" :filter="filter" />
+</template>
+
+<script>
+import CatalogFilter from "./CatalogFilter.vue";
+
+export default {
+  data() {
+    return {
+      filters: [],
+    };
+  },
+  async mounted() {
+    this.filters = ["Платформа", "Сертификация", "Язык интерфейса", "Аналоги"];
+    console.log(this.filters);
+  },
+  components: { CatalogFilter },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
