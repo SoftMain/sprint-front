46864925a0f30e0af704d97c3942a3d89387941b
Andrey Paliy <awbait.com@gmail.com>
2022-12-01 18:23:45 +0300

    change file name

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,32 +1,48 @@
 <template>
-  <header :class="isTop && this.$route.name == 'main' ? 'header_transparent' : ''">
+  <header
+    :class="isTop && this.$route.name == 'main' ? 'header_transparent' : ''"
+  >
     <div class="container">
       <nav class="nav">
         <div class="nav__wrapper">
-          <router-link to="/" class="nav__wrapper-logo" :class="isTop && this.$route.name == 'main' ? 'header_logo_color' : ''">
+          <router-link
+            to="/"
+            class="nav__wrapper-logo"
+            :class="
+              isTop && this.$route.name == 'main' ? 'header_logo_color' : ''
+            "
+          >
             <span class="icon-logo32 px32"></span>
             <h3 class="font-medium">SoftMain</h3>
           </router-link>
 
           <div @click.self="outside" class="">
-            <button  @click="isOpen = !isOpen" class="btn btn-primary btnb2" :class="isTop && this.$route.name == 'main' ? 'btnb2_transparent' : ''"
-              >Категории
-              <span class="icon-caret-down px20"></span>   
-          </button>
-          <BtnCategory v-if="isOpen"/>
+            <button
+              @click="isOpen = !isOpen"
+              class="btn btn-primary btnb2"
+              :class="
+                isTop && this.$route.name == 'main' ? 'btnb2_transparent' : ''
+              "
+            >
+              Категории
+              <span class="icon-caret-down px20"></span>
+            </button>
+            <BtnCategory v-if="isOpen" />
           </div>
         </div>
-        <div class="nav__search proto__elem">       
-            <input
-              class="search-area text-base font-regular"
-              type="search"
-              placeholder="Поиск по сайту"
-              required-placeholder=""
-            />
+        <div class="nav__search proto__elem">
+          <input
+            class="search-area text-base font-regular"
+            type="search"
+            placeholder="Поиск по сайту"
+            required-placeholder=""
+          />
           <span class="icon-search px20 search-img"></span>
         </div>
         <div class="nav__links">
-          <router-link to="/integrators" class="btn text-base font-regular btnc2"
+          <router-link
+            to="/integrators"
+            class="btn text-base font-regular btnc2"
             >Интеграторы</router-link
           >
           <router-link to="/contact" class="btn text-base font-regular btnc2"
@@ -42,12 +58,12 @@
 </template>
 
 <script>
-import BtnCategory from "./BtnCategory.vue";
+import BtnCategory from "./HeaderCategoryDropdown.vue";
 
 export default {
   components: {
-  BtnCategory
-},
+    BtnCategory,
+  },
   data() {
     return {
       isOpen: false,
