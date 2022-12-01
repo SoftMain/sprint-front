70c14602404e091a1b445403079295c4cd8c26c6
n0tc00l <113791763+n0tc00l@users.noreply.github.com>
2022-12-01 22:31:27 +0400

    ураб победа звёзд

--- src/components/StarRating.vue
+++ src/components/StarRating.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="star-rating">
-        <span  v-for="index in starLimit" :key="index" class="star star-filled"> </span>
+    <div class="info__star-rating">
+        <span  v-for="index in starLimit" :key="index" class="icon-star-filled px14"> </span>
     </div>
 </template>
 
@@ -12,7 +12,7 @@
                 type: Number,
                 default: 0
             },
-            starRating: {
+            starLimit: {
                 type: Number,
                 default: 5 
             }
@@ -21,5 +21,15 @@
 </script>
 
 <style lang="scss" scoped>
+    .info__star-rating {
+    display: flex;
+    flex-direction: row;
+    // justify-content: center;
+    align-items: center;
+    gap: 4px;
+
+    width: 137px;
+    height: 20px;
+}
 
 </style>
