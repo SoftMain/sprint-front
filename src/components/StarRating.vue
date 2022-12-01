733397294ef26b0ac1d0cad5b8165bf87bb02bd8
n0tc00l <113791763+n0tc00l@users.noreply.github.com>
2022-12-01 23:25:15 +0400

    ???

--- src/components/StarRating.vue
+++ src/components/StarRating.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="info__star-rating">
-        <span  v-for="index in starLimit" :key="index" class="icon-star-filled px14"> </span>
+        <span  v-for="index in starLimit" :key="index" class="icon-star px14"> </span>
+        <div class="star-filled">
+            <span  v-for="index in starLimit" :key="index" class="icon-star-filled px14"> </span>
+        </div>
+        <div class="product__raiting-text">(*)</div>
     </div>
 </template>
 
@@ -30,6 +34,15 @@
 
     width: 137px;
     height: 20px;
+}
+.star-filled {
+    display: inline-flex;
+    position: absolute;
+    width: 0;
+    height: 100%;
+    top: 0;
+    left: 0;
+    overflow: hidden;
 }
 
 </style>
