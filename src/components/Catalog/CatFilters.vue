580ee90fdd526d42c131a12252bab1ecb7a86ee9
Andrey Paliy <awbait.com@gmail.com>
2022-12-04 21:57:06 +0300

    change orders

--- src/components/Catalog/CatFilters.vue
+++ src/components/Catalog/CatFilters.vue
@@ -1,8 +1,76 @@
 <template>
   <div>
+
+    <!-- PLATFORM -->
+
+    <div class="filter">
+      <div @click="show3 = !show3" class="sidebar__collapse btn">
+        <div
+          :class="{
+            'sidebar__collapse-icon unrotated': show3,
+            'sidebar__collapse-icon rotated': !show3,
+          }"
+        >
+          <span class="icon-caret-down px20"></span>
+        </div>
+        <div class="sidebar__collapse-header">
+          <h6 class="font-regular">Платформа</h6>
+        </div>
+      </div>
+      <Transition name="slide-fade">
+        <div v-if="show3" class="sidebar__variants">
+          <div v-for="(platform, index) in platforms">
+            <input
+              type="checkbox"
+              :value="platform.id"
+              :id="'platform' + index"
+              v-model="selected.platforms"
+            />
+            <label :for="'platform' + index">{{ platform.name }}</label>
+          </div>
+        </div>
+      </Transition>
+    </div>
+
+    <!-- CERTIFICATES -->
+
+    <div class="filter">
+      <div @click="show2 = !show2" class="sidebar__collapse btn">
+        <div
+          :class="{
+            'sidebar__collapse-icon unrotated': show2,
+            'sidebar__collapse-icon rotated': !show2,
+          }"
+        >
+          <span class="icon-caret-down px20"></span>
+        </div>
+        <div class="sidebar__collapse-header">
+          <h6 class="font-regular">Сертификация</h6>
+        </div>
+      </div>
+      <Transition name="slide-fade">
+        <div v-if="show2" class="sidebar__variants">
+          <div v-for="(certificate, index) in certificates">
+            <input
+              type="checkbox"
+              :value="certificate.id"
+              :id="'certificate' + index"
+              v-model="selected.certificates"
+            />
+            <label :for="'certificate' + index">{{ certificate.name }}</label>
+          </div>
+        </div>
+      </Transition>
+    </div>
+
     <div class="filter">
       <div @click="show = !show" class="sidebar__collapse btn">
-        <div :class="{ 'sidebar__collapse-icon unrotated': show, 'sidebar__collapse-icon rotated': !show }">
+        <div
+          :class="{
+            'sidebar__collapse-icon unrotated': show,
+            'sidebar__collapse-icon rotated': !show,
+          }"
+        >
           <span class="icon-caret-down px20"></span>
         </div>
         <div class="sidebar__collapse-header">
@@ -27,8 +95,13 @@
     <!-- Languages -->
 
     <div class="filter">
-      <div @click="(show1 = !show1)" class="sidebar__collapse btn">
-        <div :class="{ 'sidebar__collapse-icon unrotated': show1, 'sidebar__collapse-icon rotated': !show1 }">
+      <div @click="show1 = !show1" class="sidebar__collapse btn">
+        <div
+          :class="{
+            'sidebar__collapse-icon unrotated': show1,
+            'sidebar__collapse-icon rotated': !show1,
+          }"
+        >
           <span class="icon-caret-down px20"></span>
         </div>
         <div class="sidebar__collapse-header">
@@ -49,58 +122,6 @@
         </div>
       </Transition>
     </div>
-
-    <!-- CERTIFICATES -->
-
-    <div class="filter">
-      <div @click="(show2 = !show2)" class="sidebar__collapse btn">
-        <div :class="{ 'sidebar__collapse-icon unrotated': show2, 'sidebar__collapse-icon rotated': !show2 }">
-          <span class="icon-caret-down px20"></span>
-        </div>
-        <div class="sidebar__collapse-header">
-          <h6 class="font-regular">Сертификация</h6>
-        </div>
-      </div>
-      <Transition name="slide-fade">
-        <div v-if="show2" class="sidebar__variants">
-          <div v-for="(certificate, index) in certificates">
-            <input
-              type="checkbox"
-              :value="certificate.id"
-              :id="'certificate' + index"
-              v-model="selected.certificates"
-            />
-            <label :for="'certificate' + index">{{ certificate.name }}</label>
-          </div>
-        </div>
-      </Transition>
-    </div>
-
-    <!-- PLATFORM -->
-
-    <div class="filter">
-      <div @click="(show3 = !show3)" class="sidebar__collapse btn">
-        <div :class="{ 'sidebar__collapse-icon unrotated': show3, 'sidebar__collapse-icon rotated': !show3 }">
-          <span class="icon-caret-down px20"></span>
-        </div>
-        <div class="sidebar__collapse-header">
-          <h6 class="font-regular">Платформа</h6>
-        </div>
-      </div>
-      <Transition name="slide-fade">
-        <div v-if="show3" class="sidebar__variants">
-          <div v-for="(platform, index) in platforms">
-            <input
-              type="checkbox"
-              :value="platform.id"
-              :id="'platform' + index"
-              v-model="selected.platforms"
-            />
-            <label :for="'platform' + index">{{ platform.name }}</label>
-          </div>
-        </div>
-      </Transition>
-    </div>
   </div>
 </template>
 
@@ -112,8 +133,8 @@ export default {
   data() {
     return {
       analogs: [],
-      show: true,
-      show1: true,
+      show: false,
+      show1: false,
       show2: true,
       show3: true,
       languages: [],
