d9460005ff438170cb8f53c513a76039d00b2669
Andrey Paliy <awbait.com@gmail.com>
2022-12-04 21:42:25 +0300

    update categories

--- src/components/Catalog/CatalogProducts.vue
+++ src/components/Catalog/CatalogProducts.vue
@@ -2,8 +2,11 @@
   <div class="products">
     <CatalogProduct v-for="(product, index) in products" :product="product" />
   </div>
-  <CatalogPagination :totalPages="totalPages" :page="page" @change-page="changePage"/>
-  
+  <CatalogPagination
+    :totalPages="totalPages"
+    :page="page"
+    @change-page="changePage"
+  />
 </template>
 
 <script>
@@ -12,10 +15,10 @@ import CatalogPagination from "./CatalogPagination.vue";
 
 export default {
   components: { CatalogProduct, CatalogPagination },
+  props: {
+    filterSelect: Object,
+  },
   data() {
-    props: {
-      filterSelect: Object
-    }
     return {
       products: [],
       page: 1,
@@ -25,22 +28,36 @@ export default {
   },
   methods: {
     async getProducts() {
-      const resp = await this.$axios.get('/products', {
-      params: {
-        _page: Number(this.page),
-        _limit: Number(this.limit),
-      },
-    });
-    this.products = resp.data.rows;
-    this.totalPages = Math.ceil(resp.data.count / this.limit);
+      const resp = await this.$axios.get("/products/filtered", {
+        params: {
+          _selected: {
+            analogs: this.filterSelect.analogs,
+            languages: this.filterSelect.languages,
+            platforms: this.filterSelect.platforms,
+            certificates: this.filterSelect.certificates,
+          },
+          _page: Number(this.page),
+          _limit: Number(this.limit),
+        },
+      });
+      this.products = resp.data.rows;
+      this.totalPages = Math.ceil(resp.data.count / this.limit);
     },
     changePage(page) {
       this.page = page;
       this.getProducts();
-    }
+    },
   },
   async mounted() {
     this.getProducts();
+  },
+  watch: {
+    filterSelect: {
+      handler(newValue, oldValue) {
+        this.getProducts();
+      },
+      deep: true,
+    },
   },
 };
 </script>
