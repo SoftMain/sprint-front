74313fe0575b38dcc570cd0127620b2d7e4a7abc
yanin-artem <108901382+yanin-artem@users.noreply.github.com>
2022-12-07 23:40:40 +0300

    это ожило.....(но выглядит не очень)(я про поиск)

--- src/components/Search/Search.vue
+++ src/components/Search/Search.vue
@@ -1,75 +1,78 @@
 <template>
     <div class="nav__search proto__elem">
-          <input
-            class="search-area text-base font-regular"
-            type="search"
-            placeholder="Поиск по сайту"
-            required-placeholder=""
-            @keypress="SearchTool"
-            @keyup.delete="SearchDelete"
-            v-model="message"
-          />
-          <span class="icon-search px20 search-img"></span>
+        <input class="search-area text-base font-regular" type="search" placeholder="Поиск по сайту"
+            required-placeholder="" v-model="message" @keypress="SearchTool" @keyup.delete="SearchDelete" />
+        <span class="icon-search px20 search-img"></span>
+    </div>
+    <div class="pupu">
+        <div class="pepe" v-for="item in output">
+            {{ item.name }}
         </div>
-        <div class="pupu">
-            <div class="pepe" v-for="item of output">
-                {{item.name}}
-            </div>
-        </div>
+    </div>
 </template>
 
 <script>
-    export default {
-        data(){
-            return{
-                message:'',
-                items:[   //типа db
-                    {
-                        id:1,
-                        name:"jopa"
-                    },
-                    {
-                        id:2,
-                        name:"popa"
-                    },
-                    {
-                        id:3,
-                        name:"jopapopa"
-                    },
-                    {
-                        id:4,
-                        name:"jopajopa"
-                    }
-                ],
-                output:[] //массив который будет выводиться в dropdown
-            }
-        },
-        methods:{
-            SearchTool(){
-                if(this.message.length>2){
-                    for(let item of this.items){
-                        if(item.name.includes(this.message)){ //проверка
-                            this.output.push(item)
-                        }
+export default {
+    data() {
+        return {
+            message: '',
+            items: [   //типа db
+                {
+                    id: 1,
+                    name: "jopa"
+                },
+                {
+                    id: 2,
+                    name: "popa"
+                },
+                {
+                    id: 3,
+                    name: "jopapopa"
+                },
+                {
+                    id: 4,
+                    name: "jopajopa"
+                }
+            ],
+            output: [] //массив который будет выводиться в dropdown
+        }
+    },
+    methods: {
+        SearchTool() {
+            if (this.message.length > 2) {
+                for (let item of this.items) {
+                    if (this.message.includes(item.name) || item.name.includes(this.message)) {
+                        this.output.push(item)
                     }
                 }
-                this.output=this.output.filter((item, index) => {
-                        return this.output.indexOf(item) === index  //проверка на дубликаты в списке
-                    });
-                console.log(this.output);
-            },
-            SearchDelete(){                  /////////////ВОТ ЭТО НОРМАЛЬНО НЕ РОБИТ
-                    this.output=this.output.filter((item, index) => {
-                        if(!item.name.includes(this.message)||this.message==''){
-                            this.output=this.output.splice(index-1,1);
-                            console.log(item.name);
-                        }
-                        return this.output
-                    });
-                    console.log(this.output)
+            }
+            this.output = this.output.filter((item, index) => {
+                return this.output.indexOf(item) === index
+            });
+            this.output = this.output.filter((item) => {
+                return item.name.includes(this.message);
+            });
+        },
+        SearchDelete() {
+            this.output = this.output.filter((item) => {
+                item.name.includes(this.message);
+            });
+            if (this.message.length > 2) {
+                for (let item of this.items) {
+                    if (this.message.includes(item.name) || item.name.includes(this.message)) {
+                        this.output.push(item)
+                    }
+                }
+                this.output = this.output.filter((item, index) => {
+                    return this.output.indexOf(item) === index
+                });
+            }
+            if (this.message == '') {
+                this.output.splice(0);
             }
         }
     }
+}
 </script>
 
 <style lang="scss" scoped>
