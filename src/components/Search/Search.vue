5a6b4b175f83336d14db224f89726dcd29a2c783
yanin-artem <108901382+yanin-artem@users.noreply.github.com>
2022-12-08 00:58:38 +0300

    search update

--- src/components/Search/Search.vue
+++ src/components/Search/Search.vue
@@ -1,13 +1,15 @@
 <template>
-    <div class="nav__search proto__elem">
-        <input class="search-area text-base font-regular" type="search" placeholder="Поиск по сайту"
-            required-placeholder="" v-model="message" @keypress="SearchTool" @keyup.delete="SearchDelete" />
-        <span class="icon-search px20 search-img"></span>
+    <div class="nav__search-wrapper">
+        <div class="nav__search proto__elem">
+        <input class="search-area text-base font-regular" placeholder="Поиск по сайту" required-placeholder=""
+            v-model="message" @input="ChangeIcon(),DropDown()" @keypress="SearchTool" @keyup.delete="SearchDelete" />
+        <span @click="SearchDeleteAll" class="icon-search px20 search-img"></span>
     </div>
-    <div class="pupu">
+    <div class="search__dropdown">
         <div class="pepe" v-for="item in output">
             {{ item.name }}
         </div>
+    </div>
     </div>
 </template>
 
@@ -40,38 +42,68 @@ export default {
     methods: {
         SearchTool() {
             if (this.message.length > 2) {
+                //добавляем в вывод название программы из импровизированной бд
                 for (let item of this.items) {
                     if (this.message.includes(item.name) || item.name.includes(this.message)) {
                         this.output.push(item)
                     }
                 }
             }
+            //убираем лишние повторения появившеся при итерациях
             this.output = this.output.filter((item, index) => {
                 return this.output.indexOf(item) === index
             });
+            //этот фильтр снова проверяет включает ли название проги из бд строку введенную в поиске, чтобы при точном вводе мы получали конкретный вариант
             this.output = this.output.filter((item) => {
                 return item.name.includes(this.message);
             });
         },
         SearchDelete() {
+            //этот фильтр проверяет включает ли название проги из бд строку введенную в поиске и удаляет лишнее
             this.output = this.output.filter((item) => {
                 item.name.includes(this.message);
             });
+
             if (this.message.length > 2) {
+                //все повторяется
                 for (let item of this.items) {
                     if (this.message.includes(item.name) || item.name.includes(this.message)) {
                         this.output.push(item)
                     }
                 }
+
                 this.output = this.output.filter((item, index) => {
                     return this.output.indexOf(item) === index
                 });
             }
+        },
+        //иконки меняются
+        ChangeIcon() {
             if (this.message == '') {
-                this.output.splice(0);
+                document.querySelector('.search-img').classList.remove('icon-close');
+                document.querySelector('.search-img').classList.add('icon-search');
             }
+            else {
+                document.querySelector('.search-img').classList.remove('icon-search');
+                document.querySelector('.search-img').classList.add('icon-close');
+            }
+        },
+        //при нажатии на иконку все удаляется и сама иконка меняется
+        SearchDeleteAll(){
+        this.output.splice(0);
+        this.message='';
+        document.querySelector('.search-img').classList.remove('icon-close');
+        document.querySelector('.search-img').classList.add('icon-search');
+    },
+    DropDown(){
+        if(this.output.length>0){
+            document.querySelector('.search__dropdown').classList.add('active'); 
+        }
+        else{
+            document.querySelector('.search__dropdown').classList.remove('active');
         }
     }
+    },
 }
 </script>
 
