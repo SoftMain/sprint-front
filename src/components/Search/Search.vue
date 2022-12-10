9ebc43fed05cbee5fbeb250ddf7dd07197bde19a
yanin-artem <108901382+yanin-artem@users.noreply.github.com>
2022-12-10 23:35:50 +0300

    search style

--- src/components/Search/Search.vue
+++ src/components/Search/Search.vue
@@ -2,13 +2,21 @@
     <div class="nav__search-wrapper">
         <div class="nav__search proto__elem">
         <input class="search-area text-base font-regular" placeholder="Поиск по сайту" required-placeholder=""
-            v-model="message" @input="ChangeIcon(),DropDown()" @keypress="SearchTool" @keyup.delete="SearchDelete" />
+            v-model="message" @input="ChangeIcon(),DropDown()" />
         <span @click="SearchDeleteAll" class="icon-search px20 search-img"></span>
-    </div>
-    <div class="search__dropdown">
-        <router-link to="/product" @click="SearchDeleteAll"  class="search__dropdown-item" v-for="item in output">
-            {{ item.name }}
+        <div class="search__dropdown">
+            <div class="search__dropdown-error font-regular text-base">
+                {{error_text}}
+            </div>
+        <router-link :to="'/product/' + item.id" @click="SearchDeleteAll"  class="search__dropdown-item" v-for="item in output">
+            <div class="search__dropdown-name font-regular text-base">
+                {{ item.name }}
+            </div>
+            <div class="search__dropdown-category font-regular text-sm">
+                Категория
+            </div>
         </router-link>
+    </div>
     </div>
     </div>
 </template>
@@ -18,67 +26,16 @@ export default {
     data() {
         return {
             message: '',
-            items: [   //типа db
-                {
-                    id: 1,
-                    name: "jopa"
-                },
-                {
-                    id: 2,
-                    name: "popa"
-                },
-                {
-                    id: 3,
-                    name: "jopapopa"
-                },
-                {
-                    id: 4,
-                    name: "jopajopa"
-                }
-            ],
+            error_text:'',
             output: [] //массив который будет выводиться в dropdown
         }
     },
     methods: {
-        SearchTool() {
-            if (this.message.length > 2) {
-                //добавляем в вывод название программы из импровизированной бд
-                for (let item of this.items) {
-                    if (this.message.includes(item.name) || item.name.includes(this.message)) {
-                        this.output.push(item)
-                    }
-                }
-            }
-            //убираем лишние повторения появившеся при итерациях
-            this.output = this.output.filter((item, index) => {
-                return this.output.indexOf(item) === index
-            });
-            //этот фильтр снова проверяет включает ли название проги из бд строку введенную в поиске, чтобы при точном вводе мы получали конкретный вариант
-            this.output = this.output.filter((item) => {
-                return item.name.includes(this.message);
-            });
-        },
-        SearchDelete() {
-            //этот фильтр проверяет включает ли название проги из бд строку введенную в поиске и удаляет лишнее
-            this.output = this.output.filter((item) => {
-                item.name.includes(this.message);
-            });
-
-            if (this.message.length > 2) {
-                //все повторяется
-                for (let item of this.items) {
-                    if (this.message.includes(item.name) || item.name.includes(this.message)) {
-                        this.output.push(item)
-                    }
-                }
-
-                this.output = this.output.filter((item, index) => {
-                    return this.output.indexOf(item) === index
-                });
-            }
-        },
         //иконки меняются
         ChangeIcon() {
+            // this.getProduct();
+            this.getProduct();
+
             if (this.message == '') {
                 document.querySelector('.search-img').classList.remove('icon-close');
                 document.querySelector('.search-img').classList.add('icon-search');
@@ -90,21 +47,39 @@ export default {
         },
         //при нажатии на иконку все удаляется и сама иконка меняется
         SearchDeleteAll(){
-        this.output.splice(0);
         this.message='';
         document.querySelector('.search-img').classList.remove('icon-close');
         document.querySelector('.search-img').classList.add('icon-search');
         document.querySelector('.search__dropdown').classList.remove('active');
+        document.querySelector('.search-area').classList.remove('active');
+
     },
     DropDown(){
-        if(this.message.length > 3){
+        if(this.message.length > 2){
             document.querySelector('.search__dropdown').classList.add('active'); 
+            document.querySelector('.search-area').classList.add('active'); 
         }
         else{
             document.querySelector('.search__dropdown').classList.remove('active');
+            document.querySelector('.search-area').classList.remove('active');
+
         }
+    },
+    async getProduct(){
+        const resp = await this.$axios.get(`/products/search?_string=${this.message}`, {
+      });
+      this.output=resp.data;
+      if(this.output.length==0&&this.message.length>2){
+                this.error_text='Ничего не найдено'; 
+            }
+            else{
+                this.error_text='';
+            }
     }
     },
+    async mounted() {
+    this.getProduct();
+  },
 }
 </script>
 
